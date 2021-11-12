dcb90983e41b1ba895fda3b8af0525246be23033
Ignacio Cerviño <ignacio.cervino@globant.com>
2021-11-12 14:55:21 -0300

    Adding 2 xib for the cells prototypes and making the custom chat

---
+++ ConstraintsInIB/Model/Chat.swift
@@ -0,0 +1,19 @@
+//
+//  Chat.swift
+//  ConstraintsInIB
+//
+//  Created by Ignacio Cervino on 12/11/2021.
+//
+
+import Foundation
+
+struct Chat: Decodable {
+    var username: String
+    var message: String
+    var time: String
+    
+    enum CodingKeys: String, CodingKey {
+        case username,message
+        case time = "timeMessage"
+    }
+}
